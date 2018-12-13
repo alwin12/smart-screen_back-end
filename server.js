d8b49ccc1a75057d59113833c599d5d67f5bc955
alwin sunny <alwinsunny@192-168-1-189.tpgi.com.au>
2018-12-13 19:29:01 +1100

    installed socket.io

--- server.js
+++ server.js
@@ -1,39 +1,62 @@
 
 var app = require('express')();
-var server = require ('http').Server(app); 
+var server = require ('http').Server(app);
 var io = require('socket.io')(server);
 
 
+const socketioAuth = require("socketio-auth");
+
+socketioAuth(io, { authenticate, postAuthenticate});
+
+ function authenticate(socket,data,callback){
+
+    let username = data.username;
+    let password = data.password;
+
+    if(username != client.username|| password!=client.password){
+
+
+      return callback (new Error('user not found'))
+    }
+
+   return callback(null,true);
+
+ }
+
+function postAuthenticate(client){
+
+ //emit messages
+
+
+}
+
+//  io.on('connection',(client)=>{
+//
+// client.on("hello",(data)=>{
+//
+// console.log(data)
+//
+// })
+//  })
+
+
+let client  ={
+  usernamme:'302',
+  password:'302302'
+}
 
 
 
 
 
+let PORT = process.env.port || 3002
 
 
+server.listen(PORT, ()=> {
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-server.listen(3002, ()=> {
-
-
-console.log('listening to port 3002') ;
+console.log( `listening to ${PORT}`) ;
 
 
 
 })
-
